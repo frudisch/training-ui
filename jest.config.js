acbff3453d7e756786d4ebb4fc4a26bbc170c305
frudisch <florian.rudisch@gmail.com>
2023-02-19 13:20:34 +0100

    Updated test cases and added automation with github action

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,11 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  transform: {
-    '^.+\\.(ts|tsx)?$': 'ts-jest',
-    "^.+\\.(js|jsx)$": "babel-jest",
-  },
-  transformIgnorePatterns: ['<rootDir>/node_modules/'],
-  moduleNameMapper: {
-    "axios": "axios/dist/node/axios.cjs"
-  }
+    preset: 'ts-jest',
+    moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
+    transformIgnorePatterns: ['<rootDir>/node_modules/'],
+    moduleNameMapper: {
+        "axios": "axios/dist/node/axios.cjs",
+        "\\.(css|less|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
+        "\\.(gif|ttf|eot|svg)$": "<rootDir>/__mocks__/fileMock.js"
+    }
 };
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import App from './App';
+import * as selectEvents from "./api/selectEvents";
 
-test('should render the App and match the snapshot', () => {
-  const rendered = renderer
-      .create(<App></App>)
-      .toJSON();
-  expect(rendered).toMatchSnapshot();
-});
+jest.mock('./api/selectEvents');
+const selectEventsMock = selectEvents as jest.Mocked<typeof selectEvents>;
+describe('App', () => {
+    it('should be rendered and match the snapshot', async () => {
+        const expectedValues = [{
+            date: '03.01.2023',
+            end: '20:00',
+            start: '18:00',
+        }, {
+            date: '07.01.2023',
+            end: '20:00',
+            start: '18:00',
+            enemy: 'Füchse Berlin'
+        }];
+        selectEventsMock.default.mockResolvedValue(expectedValues);
+
+        const rendered = renderer
+            .create(<App></App>)
+            .toJSON();
+
+        expect(rendered).toMatchSnapshot();
+    });
+})
